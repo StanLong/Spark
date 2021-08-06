e30609fd55a9e6a2942b81c2deb71f92adabfa8c
StanLong <youlinbhz@sina.cn>
2021-08-06 22:30:03 +0800

    依赖关系

---
+++ Spark/spark-core/src/main/java/com/stanlong/spark/core/rdd/dep/Spark01_Rdd_dep.scala
@@ -0,0 +1,37 @@
+package com.stanlong.spark.core.rdd.dep
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_Rdd_dep {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local").setAppName("WordCount")
+        val sc = new SparkContext(sparkConf)
+
+        val lines: RDD[String] = sc.textFile("datas/1.txt")
+        println(lines.toDebugString) // toDebugString 打印血缘关系
+        println("/*************************")
+
+        val words: RDD[String] = lines.flatMap(_.split(" "))
+        println(words.toDebugString)
+        println("/*************************")
+
+        val wordToOne = words.map(word=>(word,1))
+        println(wordToOne.toDebugString)
+        println("/*************************")
+
+        val wordToSum: RDD[(String, Int)] = wordToOne.reduceByKey(_+_)
+        println(wordToSum.toDebugString)
+        println("/*************************")
+
+        val array: Array[(String, Int)] = wordToSum.collect()
+        array.foreach(println)
+
+
+        // 关闭连接
+        sc.stop()
+    }
+
+}
