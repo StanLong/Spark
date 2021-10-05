cd0d4c1c441c6cbb46bf1afe59b907d9e4413765
StanLong <youlinbhz@sina.cn>
2021-10-05 19:07:27 +0800

    分区及数据保存规则

--- Spark/spark-core/src/main/java/com/stanlong/spark/core/rdd/builder/Spark03_RDD_File_Par.scala
+++ Spark/spark-core/src/main/java/com/stanlong/spark/core/rdd/builder/Spark03_RDD_File_Par.scala
@@ -10,9 +10,31 @@ object Spark03_RDD_File_Par {
 
         val sc = new SparkContext(spakConf)
 
-        // textFile 默认两个分区
-        val rdd = sc.textFile("datas/1.txt")
-        // val rdd = sc.textFile("datas/1.txt", 3) 也可以手动指定分区数，比如指定三个分区
+        // textFile
+        // 第一个参数是文件路径，第二个参数是分区数量，默认两个分区
+        // val rdd = sc.textFile("datas/1.txt")
+        val rdd = sc.textFile("datas/1.txt", 3) // 也可以手动指定分区数，比如指定三个分区
+
+        // 分区数量的计算方式
+        // 文件 totalSize
+        // goalSize = totalSize / (numSplits == 0 ? 1 : numSplits) 分区数量，如果余数大于商的10%， 则会新增一个分区
+        // 24/3 = 8(字节)
+        // 24/8=3..0(分区)
+
+        // 分区数据的计算方式
+        // 1. spark 读取文件，采用的是hadoop方式读取，所以一行一行的读，和字节数没有关系
+        // 2. 读取数据时以偏移量为单位， 偏移量不会被重新读取
+        /**
+         * Hello World@@ => 偏移量 0-12
+         * Hello Scala => 偏移量 13-23
+         */
+        // 3. 数据分区的偏移量范围的计算
+        // 0 => [0, 8] =》 Hello World@@
+        // 1 => [8, 16] =》Hello Scala
+        // 2 => [16,24] =》空
+
+
+
 
         // 将处理的数据保存成分区文件
         rdd.saveAsTextFile("output")
